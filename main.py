36d669cb4c1a88da5c24da5d2a6bb7cc121741a2
my-crypto-bot-py <mehtabs1994@gmail.com>
2026-07-11 22:41:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 import os
 import telebot
-from flask import Flask, request
 
 TOKEN = os.environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return "Bot is running!", 200
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Bot is running in Polling Mode!")
 
-@app.route(f'/{TOKEN}', methods=['POST'])
-def webhook():
-    json_str = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return '', 200
-
-# Gunicorn ke liye main block ki zaroorat nahi hai
+# Infinite polling: Bot kabhi band nahi hoga, Railway bas script chalaye rakhega
+print("Bot is polling...")
+bot.infinity_polling()
