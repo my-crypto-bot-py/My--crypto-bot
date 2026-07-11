f6143e861a70d6f1240ccf49798aaf9d87c273d3
my-crypto-bot-py <mehtabs1994@gmail.com>
2026-07-11 22:30:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,9 @@ def index():
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        try:
-            json_str = request.get_data().decode('utf-8')
-            update = telebot.types.Update.de_json(json_str)
-            bot.process_new_updates([update])
-            return '', 200
-        except Exception as e:
-            return str(e), 500
-    return 'Forbidden', 403
+    json_str = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return '', 200
 
-if __name__ == "__main__":
-    # Local development ke liye
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# Gunicorn ke liye main block ki zaroorat nahi hai
