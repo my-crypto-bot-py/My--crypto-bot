c1914b0547b7315c9e2209c28f62c3ed2af6cfb5
my-crypto-bot-py <mehtabs1994@gmail.com>
2026-07-14 13:37:50 +0530

    Update structure.py

--- structure.py
+++ structure.py
@@ -1,1 +1,24 @@
+import pandas as pd
 
+def find_swings(df, left=2, right=2):
+    """
+    Swing High aur Swing Low detect karta hai.
+    """
+
+    highs = []
+    lows = []
+
+    for i in range(left, len(df) - right):
+
+        high = df["high"].iloc[i]
+        low = df["low"].iloc[i]
+
+        # Swing High
+        if high == max(df["high"].iloc[i-left:i+right+1]):
+            highs.append((i, high))
+
+        # Swing Low
+        if low == min(df["low"].iloc[i-left:i+right+1]):
+            lows.append((i, low))
+
+    return highs, lows
