fbd94ee924b8e7ad239bf8373f7c45002506ab86
my-crypto-bot-py <mehtabs1994@gmail.com>
2026-07-14 18:23:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 import os
 import telebot
-from market import get_liquidation_data
+from market import get_market_data
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
-if not TOKEN or not CHAT_ID:
-    print("Error: TOKEN or CHAT_ID missing!")
-    exit()
-
 bot = telebot.TeleBot(TOKEN)
 
 def run():
-    try:
-        print(f"Testing connection to CHAT_ID: {CHAT_ID}")
 
-        # CoinGlass API Test
-        data = get_liquidation_data("BTCUSDT")
+    try:
+
+        data = get_market_data("BTCUSDT")
+
+        if data is None:
+            bot.send_message(
+                int(CHAT_ID),
+                "❌ Market Data Fetch Failed."
+            )
+            return
 
         bot.send_message(
             int(CHAT_ID),
-            f"✅ Bot Online\n\nCoinGlass API Connected Successfully\n\nResponse:\n{str(data)[:500]}"
+            f"✅ Market Connected\n\n{str(data)[:1000]}"
         )
 
-        print("Success!")
+        print("Success")
 
     except Exception as e:
-        print(f"Error: {e}")
+
+        print(e)
+
         bot.send_message(
             int(CHAT_ID),
-            f"❌ Error:\n{e}"
+            f"❌ Error\n\n{e}"
         )
+
 
 if __name__ == "__main__":
     run()
