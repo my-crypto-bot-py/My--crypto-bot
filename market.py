80f2ad33190f77883f2b1146bfd0a4ebd69aa9d8
my-crypto-bot-py <mehtabs1994@gmail.com>
2026-07-14 18:44:19 +0530

    Update market.py

--- market.py
+++ market.py
@@ -58,3 +58,15 @@ def get_market_data(symbol="BTC-USDT-SWAP", timeframe="5m", limit=100):
     except Exception as e:
         print(e)
         return None
+def get_multi_timeframe_data(symbol="BTC-USDT-SWAP"):
+
+    timeframes = {
+        "1d": get_market_data(symbol, "1D"),
+        "4h": get_market_data(symbol, "4H"),
+        "1h": get_market_data(symbol, "1H"),
+        "30m": get_market_data(symbol, "30m"),
+        "15m": get_market_data(symbol, "15m"),
+        "5m": get_market_data(symbol, "5m"),
+    }
+
+    return timeframes
