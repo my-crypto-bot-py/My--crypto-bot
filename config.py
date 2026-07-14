2920283ddf10892cfd5a11e0a6e85d7835621552
my-crypto-bot-py <mehtabs1994@gmail.com>
2026-07-14 12:46:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import os
 
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+COINGLASS_API_KEY = os.getenv("COINGLASS_API_KEY")
+
+SYMBOLS = ["BTC/USDT", "ETH/USDT", "SOL/USDT", "XRP/USDT"]
+
+TIMEFRAMES = {
+    "trend": "4h",
+    "bias": "1h",
+    "setup": "15m",
+    "entry": "5m"
+}
