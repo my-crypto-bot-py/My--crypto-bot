037b93c8e877ebe52933493281cad9b70bd19e51
my-crypto-bot-py <mehtabs1994@gmail.com>
2026-07-15 10:46:50 +0530

    Update market.py

--- market.py
+++ market.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 
-def get_market_data(symbol="BTC-USDT-SWAP", timeframe="5m", limit=100):
+def get_market_data(symbol="BTC-USDT-SWAP", timeframe="5m", limit=300):
 
     url = "https://www.okx.com/api/v5/market/candles"
 
@@ -14,15 +14,18 @@ def get_market_data(symbol="BTC-USDT-SWAP", timeframe="5m", limit=100):
 
     try:
 
-        r = requests.get(url, params=params, timeout=20)
+        response = requests.get(url, params=params, timeout=20)
 
-        data = r.json()
+        data = response.json()
 
         if data.get("code") != "0":
-            print(data)
+            print("OKX Error:", data)
             return None
 
         candles = data["data"]
+
+        if not candles:
+            return None
 
         df = pd.DataFrame(
             candles,
@@ -39,34 +42,39 @@ def get_market_data(symbol="BTC-USDT-SWAP", timeframe="5m", limit=100):
             ]
         )
 
-        df = df[["time", "open", "high", "low", "close", "volume"]]
+        df = df[[
+            "time",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume"
+        ]]
 
-        df["time"] = pd.to_datetime(df["time"].astype("int64"), unit="ms")
+        df["time"] = pd.to_datetime(
+            df["time"].astype("int64"),
+            unit="ms"
+        )
 
-        df = df.astype({
-            "open": float,
-            "high": float,
-            "low": float,
-            "close": float,
-            "volume": float
-        })
+        for col in ["open", "high", "low", "close", "volume"]:
+            df[col] = df[col].astype(float)
 
-        df = df.sort_values("time")
+        df = df.sort_values("time").reset_index(drop=True)
 
         return df
 
     except Exception as e:
-        print(e)
+        print("Market Error:", e)
         return None
+
+
 def get_multi_timeframe_data(symbol="BTC-USDT-SWAP"):
 
-    timeframes = {
-        "1d": get_market_data(symbol, "1D"),
-        "4h": get_market_data(symbol, "4H"),
-        "1h": get_market_data(symbol, "1H"),
-        "30m": get_market_data(symbol, "30m"),
-        "15m": get_market_data(symbol, "15m"),
-        "5m": get_market_data(symbol, "5m"),
+    return {
+        "1D": get_market_data(symbol, "1D", 300),
+        "4H": get_market_data(symbol, "4H", 300),
+        "1H": get_market_data(symbol, "1H", 300),
+        "30m": get_market_data(symbol, "30m", 300),
+        "15m": get_market_data(symbol, "15m", 300),
+        "5m": get_market_data(symbol, "5m", 300),
     }
-
-    return timeframes
