8d30bd72e107f4419149a2009d7f0d08153b58c4
my-crypto-bot-py <mehtabs1994@gmail.com>
2026-07-22 17:49:12 +0530

    Update structure_v12.py

--- structure_v12.py
+++ structure_v12.py
@@ -45363,8 +45363,10 @@ def v12_final_signal_gate_v12(
 # MAIN.PY COMPATIBILITY
 # ==========================
 def get_v12_master_signal_v12(df) -> Dict:
-    print("globals gate:", "v12_final_signal_gate_v12" in globals())
-    print("gate object:", globals().get("v12_final_signal_gate_v12"))
-    return globals()["v12_final_signal_gate_v12"](df)
+
+    print("FUNCTION STARTED")
+    print("LOCAL CHECK:", "v12_final_signal_gate_v12" in globals())
+
+    return v12_final_signal_gate_v12(df)
 
     
